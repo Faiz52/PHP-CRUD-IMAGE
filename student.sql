cdfde540f3a04e32adf7edcdc9aa871d364899c3
Faizan ur Rehman <faizan.rehman52@gmail.com>
2019-07-17 06:01:02 +0500

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 15, 2018 at 01:08 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Jul 17, 2019 at 02:59 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,18 +30,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `student` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `batch` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `name` varchar(191) NOT NULL,
+  `batch` varchar(191) NOT NULL,
+  `email` varchar(191) NOT NULL,
+  `image` varchar(191) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id`, `name`, `batch`, `email`) VALUES
-(3, 'John Doe', 'abc-123', 'john@email.com'),
-(4, 'John Smith', 'abc-124', 'johns@email.com');
+INSERT INTO `student` (`id`, `name`, `batch`, `email`, `image`) VALUES
+(1, 'Johndoe', 'ab-123', 'johndoe@gmail.com', 'Skull-Wings.png');
 
 --
 -- Indexes for dumped tables
@@ -59,7 +61,9 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
